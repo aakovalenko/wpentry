b59c9571f3c048d1facdb87705c81ac09bcff0c9
andrii <kaa89pl@gmai.com>
2021-06-12 19:02:09 +0300

    add 404 page

--- wp-content/themes/seventycinema/index.php
+++ wp-content/themes/seventycinema/index.php
@@ -5,8 +5,17 @@ get_header();
 
 ?>
 
+<div>
+    <?php
 
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            the_content();
+        endwhile;
+    endif;
+    ?>
 
+</div>
 
 
 <?php
